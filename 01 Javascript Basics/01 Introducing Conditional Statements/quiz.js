ba1814abd1442e5188ce130959376472a2d64f30
MichaelPay <Michael.Pay@mail.com>
2018-01-01 23:09:52 -0800

    01 - JavaScript Basics 02 - Building a Random Number Guessing Game

---
+++ 01 Javascript Basics/01 Introducing Conditional Statements/quiz.js
@@ -0,0 +1,8 @@
+var answer = prompt('What is the best programming lanugage?')
+if (answer.toUpperCase() === 'JAVASCRIPT') {
+    alert('You are correct!')
+    document.write('You are correct!')
+} else {
+    alert('Javascript is the best language!')
+    document.write('Javascript is the best language!')
+}
